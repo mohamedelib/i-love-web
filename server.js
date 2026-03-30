bc30fbf7e10ab3d633a82ee9c13e673c585fac8c
Ikkezwam <mohamedelibrahymy374@gmail.com>
2026-03-30 10:16:09 +0200

    .

---
+++ server.js
@@ -0,0 +1,53 @@
+import express from "express";
+
+// Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
+import { Liquid } from "liquidjs";
+
+// Je kunt de volgende URLs uit onze API gebruiken:
+// - https://fdnd.directus.app/items/tribe
+// - https://fdnd.directus.app/items/squad
+// - https://fdnd.directus.app/items/person
+// En combineren met verschillende query parameters als filter, sort, search, etc.
+// Gebruik hiervoor de documentatie van https://directus.io/docs/guides/connect/query-parameters
+// En de oefeningen uit https://github.com/fdnd-task/connect-your-tribe-squad-page/blob/main/docs/squad-page-ontwerpen.md
+
+const app = express();
+const messages = [];
+
+app.use(express.static("public"));
+
+// Stel Liquid in als 'view engine'
+const engine = new Liquid();
+app.engine("liquid", engine.express());
+
+// Stel de map met Liquid templates in
+// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
+app.set("views", "./views");
+
+// Zorg dat werken met request data (volgende week) makkelijker wordt
+app.use(express.urlencoded({ extended: true }));
+
+// Stel het poortnummer in waar express op moet gaan luisteren
+app.set("port", process.env.PORT || 8000);
+
+// Start express op, haal daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get("port"), function () {
+  // Toon een bericht in de console en geef het poortnummer door
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
+
+app.get("/", async function (request, response) {
+  // Render index.liquid uit de views map en geef de opgehaalde data mee als variabele, genaamd persons
+  // Geef ook de eerder opgehaalde squad data mee aan de view
+  response.render("messages.liquid", {
+    messages: messages,
+  });
+});
+
+app.get("/expir", async function (request, response) {
+  // Render index.liquid uit de views map en geef de opgehaalde data mee als variabele, genaamd persons
+  // Geef ook de eerder opgehaalde squad data mee aan de view
+  response.render("expir.liquid", {
+    messages: messages,
+  });
+});
